c7928164594462612fec348067bd449b4f3e9e72
Nishaat Rajabali <nishaat.rajabali@hazelcast.com>
2025-09-05 13:30:59 +0100

    changes

--- export-utilities/scripts/test-export-utils.sh
+++ export-utilities/scripts/test-export-utils.sh
@@ -2,11 +2,7 @@
 
 UTILS_PATH=$1
 
-echo "XXXXXXXXX ${UTILS_PATH}"
-source <(cat ${UTILS_PATH}/*.sh)
+source ${UTILS_PATH}/logging.functions.sh.sh
 
-# echonotice "I am running test-export-utils.sh"
-# echowarning "I am running test-export-utils.sh"
-# log_header "Tests HEADER in test-export-utils.sh"
-# my_func1
-# my_func2
+echonotice "I am running test-export-utils.sh"
+echowarning "I am running test-export-utils.sh"
