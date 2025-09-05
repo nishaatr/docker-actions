88776694e322a58d28d118320eed73cc56923b0e
Nishaat Rajabali <nishaat.rajabali@hazelcast.com>
2025-09-05 13:08:10 +0100

    changes

---
+++ export-utilities/scripts/test-export-utils.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -eu ${RUNNER_DEBUG:+-x}
+
+UTILS_PATH=$1
+
+source <(cat ${UTILS_PATH}/*.sh)
+
+echonotice "I am running test-export-utils.sh"
+echowarning "I am running test-export-utils.sh"
+log_header "Tests HEADER in test-export-utils.sh"
+my_func1
+my_func2
